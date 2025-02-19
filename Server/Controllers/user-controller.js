174edad67a47427fdb23ac6b12c37f0ec77a9163
Praveenkumar0101 <praveenkumar.vancharla@gmail.com>
2025-02-19 15:57:09 +0530

    user model updated

---
+++ Server/Controllers/user-controller.js
@@ -0,0 +1,13 @@
+const bcrypt = require ('bcryptjs')
+const user = require('../Models/userModel')
+require('dotenv').config();
+
+
+
+exports.register = async , (req, res)=>{
+    try{
+        const {email, password,role}= req.body;
+
+       
+    }
+}
