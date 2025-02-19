5c61ee572574251d4fd4bac9048016461dcec37c
Vishnu Bandari <tridizi.dev2@gmail.com>
2025-02-19 14:35:05 +0530

    new changes-main

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,22 @@
-let a = 10;
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const sequelize = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => console.log(`running on http://localhost:${PORT}`));
+  })
+  .catch((err) => console.log("DB Connection Error:", err));
