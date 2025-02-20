76097d3da279e90f432c07c9c0d3ba44db4cd917
Kaapu Swetha <k.swethareddy23@gmail.com>
2025-02-20 12:03:59 +0530

    swetha commit

--- Server/app.js
+++ Server/app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const sequelize = require("./config/db");
+const {sequelize} = require("./Config/DBconnection");
+const auditLogsRouter=require("./Routes/audit-logs-route")
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use("/api",auditLogsRouter)
 
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+
 
 sequelize
   .sync()
