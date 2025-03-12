63007cea176a8485d69255315acc2c2df7b751ed
SigatapuSaiSankar <111736268+SigatapuSaiSankar@users.noreply.github.com>
2025-03-12 23:20:20 +0530

    Update DBconnection.js

--- Server/Config/DBconnection.js
+++ Server/Config/DBconnection.js
@@ -1,44 +1,22 @@
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//   }
-// );
-
-// try {
-//   sequelize.authenticate();
-//   console.log("Connection has been established successfully.");
-// } catch (error) {
-//   console.error("Unable to connect to the database:", error);
-// }
-
-// module.exports = sequelize;
-
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    timezone: "+05:30",
-    port: 24364,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Adjust this as per your security needs
-      },
-    },
+    dialect: "postgres",
   }
 );
 
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
 module.exports = sequelize;
+
