921c299d70569d29788acb43e08113aa40735933
tarun kumar ponnada <saiponnada46@gmail.com>
2025-03-18 11:18:26 +0530

    commit for db connection

--- Server/Config/DBconnection.js
+++ Server/Config/DBconnection.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+// Db connection with psql
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
